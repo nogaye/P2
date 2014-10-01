ca8e4623d1e6c860cf2ec33dc38a22c7d9312d9a
Nicholas Ogaye <owallaa@yahoo.com>
2014-09-30 21:31:20 -0400

    Added index.php and logic.php
    Added index.php and logic.php

---
+++ index.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Nicholas Ogaye - CSCI-E15 Project 2</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"/>
+
+  
+
+    <style>
+        img {
+            display: block;
+            margin: auto;
+            width: 20%;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .t-circle {
+            color: white;
+            background-color: #428bca;
+            outline-color: #428bca;
+            border-radius: 80px;
+            text-align: center;
+            padding: 10px;
+            width: 10px;
+            height: 10px;
+            margin: 5px;
+        }
+
+        .t-title {
+            color: white;
+            background-color: #428bca;
+            border-radius: 80px;
+            text-align: center;
+            padding: 20px;
+            margin: 10px;
+        }
+
+    </style>
+</head>
+<body>
+     <?php require('logic.php');?>
+    <div class="container">
+       
+        <div class="t-title">
+
+            <h2> Nicholas Ogaye </h2>
+
+            <h3>
+                <b>
+                    An xkcd style password generator
+                </b>
+            </h3>
+
+        </div>
+      
+
+   
+
+       
+
+        </div>
+</body>
+</html>
+
+
+
+
+
